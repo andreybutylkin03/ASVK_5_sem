c56ae8a7b273910503348c5eb3a0772a0c39b470
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej.local>
2023-10-24 14:14:22 +0300

    be here

--- 20231024/0/prog8.py
+++ 20231024/0/prog8.py
@@ -1,5 +1,4 @@
 import itertools
 
-res = list(itertools.product("abcdefgh", map(str, range(1, 9))))
-for i, j in res:
-    print(f"{i}{j}")
+print(*[f"{i}{j}" for i, j in list(itertools.product("abcdefgh", map(str, range(1, 9))))], sep='\n')
+
