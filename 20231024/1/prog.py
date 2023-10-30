b02f974bf0240bb4ffdce943ce584ccfe218bacc
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej.local>
2023-10-30 23:20:10 +0300

    do hw1

--- 20231024/1/prog.py
+++ 20231024/1/prog.py
@@ -1,13 +1,12 @@
 import itertools
 
-class A:
-    def fib(self, m, n):
-        def vs(a=1, b=0):
-            while True:
-                yield a
-                a, b = a + b, a
+def fib(m, n):
+    def vs(a=1, b=0):
+        while True:
+            yield a
+            a, b = a + b, a
 
-        return itertools.islice(vs(), m, m + n)
+    return itertools.islice(vs(), m, m + n)
 
 import sys
 exec(sys.stdin.read())
