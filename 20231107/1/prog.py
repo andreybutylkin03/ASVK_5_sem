11cade3ab3c850c0402223f9e12bf0ade9c69897
Андрей Бутылкин <a.s.butylkin@MacBook-Air-Andrej.local>
2023-11-11 10:55:44 +0300

    do hw1

---
+++ 20231107/1/prog.py
@@ -0,0 +1,33 @@
+import collections
+
+class DivStr(collections.UserString):
+    def __init__(self, st='', step=0, kol=1):
+        super().__init__(st)
+        self.step = step
+        self.st = st
+        self.si = len(st)
+        self.i = 0
+        self.kol = kol
+
+    def __floordiv__(self, num):
+        self.kol = num
+        return DivStr(self.st, self.si // self.kol, self.kol)
+
+    def __mod__(self, num):
+        vr = divmod(self.si, num)
+        return DivStr('') if vr[1] == 0 else DivStr(self.st[-vr[1]:])
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        if self.kol == 0:
+            raise StopIteration
+        else:
+            self.kol -= 1
+            self.i += self.step
+            return self.st[self.i - self.step: self.i]
+
+import sys
+exec(sys.stdin.read())
